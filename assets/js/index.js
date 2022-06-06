dbe958117df0b8cd834b57b8ab1a77f079fc150e
Vincent Langat <kimutailangat033@gmail.com>
2022-06-06 23:51:07 +0300

    will link js to the index file to enable user to send an email

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// function to send me an email
+function sendEmail() {
+    var link = "mailto:kimutailangat033@gmail.com"
+             + "?Bcc=info@nerdware@gmail.com"
+             + "&subject=" + encodeURIComponent("ENQUIRY ABOUT")
+             + "&body=" + encodeURIComponent ("Hello, I hope you are doing well, I would like to make an enquiry about ..") // (document.getElementById('myText').value)
+    ;
+    
+    window.location.href = link;
+  }
+  
